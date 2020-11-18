fa6481d9bf8d5704adebb8b95783aeb12eb23a5e
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-11-17 21:41:46 -0500

    done

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ let unleashDog = function unleashDog(dogName, dogBreed) {
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
 
 function exerciseDog(dog, breed) {
-    return routine.map(fn => fn(dog, breed))
+    return routine.map
 }
